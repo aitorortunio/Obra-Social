6d6dba7188dc13ed721ca814e6f7fba10b1c4c56
elikohon <elikohon@gmail.com>
2021-10-07 09:21:42 -0300

    sda

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,21 +21,21 @@ class UserSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'role_id' => Role::where('name','admin')->first()->id,
-            'password' => bcrypt('1234'),
+            'password' => bcrypt('123445678'),
         ));
 
         User::create(array(
             'name' => 'empleado',
             'email' => 'empleado@empleado.com',
             'role_id' => Role::where('name','empleado')->first()->id,
-            'password' => bcrypt('1234'),
+            'password' => bcrypt('12345678'),
         ));
 
         User::create(array(
             'name' => 1,
             'email' => 'afiliado@afiliado.com',
             'role_id' => Role::where('name','afiliado')->first()->id,
-            'password' => bcrypt('1234'),
+            'password' => bcrypt('12345678'),
         ));
     }
 }
