5b28b026d925a388093279c03adb290f743dd69e
elikohon <elikohon@gmail.com>
2021-10-20 14:38:15 -0300

    visualizacion de planes

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plan;
+use App\Models\PlanPrestation;
+use App\Models\Prestation;
 use Illuminate\Http\Request;
+
 
 class PlanController extends Controller
 {
     //
     public function show(){
-       return view('afiliate.miplan');
+        $planes = Plan::all();
+        //id, nombre
+        //En el lugar del plan id, tengo porcentaje, plan_id, prestation_id
+        $planConPrestaciones = array();
+        //En el lugar del plan id, tengo el nombre de una prestacion
+        $prestacionesDelPlan = array();
+        foreach($planes as $plan){
+            $prestaciones = PlanPrestation::where('plan_id', '=', $plan->id)->get();
+            $planConPrestaciones[$plan->id] = $prestaciones;
+            
+            $prest = array();
+            foreach($prestaciones as $p){
+                $nombrep = Prestation::findOrFail($p->prestation_id);
+                $prest[] = $nombrep;
+
+            }
+            $prestacionesDelPlan[$plan->id] = $prest;
+            
+        }
+
+        
+
+        return view('afiliate.miplan')->with(['planP'=>$planConPrestaciones, 'prestP'=>$prestacionesDelPlan, 'planes'=>$planes]);
     }
+
+
 }
--- resources/views/afiliate/miplan.blade.php
+++ resources/views/afiliate/miplan.blade.php
@@ -3,40 +3,30 @@
 <head>
 <h1 align=center>Plan</h1>
 </head>
-<br>
-<br>
-<br>
 <div class="row">
-&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <!--Es un espacio (Es rancio lo se pero bueno para safar alcanza jeje)-->
+@foreach($planes as $plan)
     <div class="card text-white bg-dark mb-3" align=center style="width: 18rem;">
+    
     <div class="card-body">
-            <h1 class="card-title" align=center >Plan A</h1>
-            <h4  align=center>Prestacion 1</h4>
-            <h4  align=center>Prestacion 2</h4>
-            <h4  align=center>Prestacion 3</h4>
-            <a href="#" class="btn btn-secondary center">Mas info</a>
+        
+            <h1 class="card-title" align=center >{{$plan->name}}</h1>
+            
+                @foreach($prestP[$plan->id] as $pp)
+                    @foreach($planP[$plan->id] as $porcent)
+                        @if($porcent->prestation_id == $pp->id)
+                        <div class="col">
+                            <h4  align=center>{{$pp->name}}</h4> 
+                            <h3  align=center>{{$porcent->percentage}} %</h3>
+                        </div>
+                        @endif
+                    @endforeach
+                @endforeach
+                
+        
+            
         </div>
     </div>
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-    <div class="card text-white bg-dark mb-3" align=center style="width: 18rem;">
-        <div class="card-body">
-            <h1 class="card-title" align=center>Plan B</h1>
-            <h4  align=center>Prestacion 1</h4>
-            <h4  align=center>Prestacion 2</h4>
-            <h4  align=center>Prestacion 3</h4>
-            <a href="#" class="btn btn-secondary">Mas info</a>
-        </div>
-    </div>
-    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-    <div class="card text-white bg-dark mb-3" align=center style="width: 18rem;">
-    <div class="card-body">
-            <h1 class="card-title" align=center>Plan C</h1>
-            <h4  align=center>Prestacion 1</h4>
-            <h4  align=center>Prestacion 2</h4>
-            <h4  align=center>Prestacion 3</h4>
-            <a href="#" class="btn btn-secondary">Mas info</a>
-        </div>
-    </div>
+    @endforeach
 </div>
     <div >
       <a href="{{route('dashboard')}}" class="shadow bg-purple-900 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-2 "><i class="fa fa-pencil mr-1"></i>Volver</a>
