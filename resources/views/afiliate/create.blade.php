0bff44e97a1503a2e2f4cbf276b94130e1e43fb7
lucasch98 <lucasch98.lch@gmail.com>
2021-10-21 22:40:11 -0300

    cambios

--- resources/views/afiliate/create.blade.php
+++ resources/views/afiliate/create.blade.php
@@ -114,8 +114,8 @@
                 
           <!-- buttons -->
           <div class="form-group">
-            <button class="shadow bg-purple-900 hover:bg-purple-900 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded " >Siguiente</button>
-            <a href="{{route('/')}}" onclick="return confirm('¿Desea cancelar la operacion?')" class="shadow bg-purple-900 hover:bg-purple-900 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded ">Cancelar</a> 
+            <button class="btn btn-dark" >Siguiente paso</button>
+            <a href="{{route('/')}}" onclick="return confirm('¿Desea cancelar la operacion?')" class="btn btn-dark">Cancelar</a> 
           </div>
     </div>
   </form>
